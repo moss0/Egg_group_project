85d9129fcf278b250670286c6136e7fd41fc3ce8
moss0 <2317919@students.ucreative.ac.uk>
2025-02-03 22:16:11 +0000

    prefab cleaning

--- EggPlatformer/Assets/Scripts/A_to_B_Object.cs
+++ EggPlatformer/Assets/Scripts/A_to_B_Object.cs
@@ -9,13 +9,13 @@ public class A_to_B_Object : MonoBehaviour
     [SerializeField] private float speed = 1;
     private float sinTime;
     private Vector3 a, b, storage;
-    public GameObject[] objs;
+    //public GameObject[] objs;
     private void Start()
     {
         a = transform.position;
         b = target.transform.position;
-        objs[0] = target;
-        objs[1] = gameObject;
+        //objs[0] = target;
+        //objs[1] = gameObject;
     }
     private void Update()
     {
