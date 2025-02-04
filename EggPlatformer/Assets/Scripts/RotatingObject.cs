b17b34d1f12262f00158e345e94e8355a43187d3
moss0 <2317919@students.ucreative.ac.uk>
2025-02-04 21:27:52 +0000

    level2 done

--- EggPlatformer/Assets/Scripts/RotatingObject.cs
+++ EggPlatformer/Assets/Scripts/RotatingObject.cs
@@ -6,7 +6,6 @@ public class RotatingObject : MonoBehaviour
 {
     public Vector3 rotationDirection = Vector3.up;
     public float rotationSpeed = 50f;
-
     private void Update()
     {
         transform.Rotate(rotationDirection.normalized, rotationSpeed * Time.deltaTime);
